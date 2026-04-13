3001bb82365fcdce0d3bab41a27175f04cb1b91e
Stephan Kern <stephan@swmmrman.com>
2026-04-13 01:47:18 -0600

    Wait for and check connection.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from machine import Pin, SoftI2C, 
 from lcd_with_i2c import LCD
+from time import sleep
 import network, wifi
 
 
@@ -9,4 +10,11 @@ lcd.puts("Hello World.")
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(wifi.ssid, wifi.password)
+
+print()
+time_waiting = 0
+while wlan.status() != 3:
+    print(f"\rWaiting for wifi: {time_waiting} seconds", end="")
+    time_waiting = time_waiting + 1
+    sleep(1)
 
