2997b187c71a993b7a73fc306297ec85c40ebb78
Stephan Kern <stephan@swmmrman.com>
2026-04-13 01:38:45 -0600

    Connect to wifi

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from machine import Pin, SoftI2C
+from machine import Pin, SoftI2C, 
 from lcd_with_i2c import LCD
+import network, wifi
+
 
 lcd = LCD(SoftI2C(scl=Pin(1), sda=Pin(0), freq=100000))
 lcd.puts("Hello World.")
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(wifi.ssid, wifi.password)
+
